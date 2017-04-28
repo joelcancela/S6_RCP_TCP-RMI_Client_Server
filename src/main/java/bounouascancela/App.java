6c6339b1ec848c549148dd35abe80a2d6d133aea
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-04-28 10:45:14 +0200

    Initial commit

---
+++ src/main/java/bounouascancela/App.java
@@ -0,0 +1,13 @@
+package bounouascancela;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
