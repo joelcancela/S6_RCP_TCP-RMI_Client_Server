4444c1c5fe2488f110e3c702893e08fa7600d41e
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-04-28 11:44:00 +0200

    CommandHelp / Command

---
+++ src/main/java/bounouascancela/sharedobjects/Command.java
@@ -0,0 +1,8 @@
+package bounouascancela.sharedobjects;
+
+/**
+ * Created by Nassim B. on 28/04/17.
+ */
+public abstract class Command
+{
+}
---
+++ src/main/java/bounouascancela/sharedobjects/CommandHelp.java
@@ -0,0 +1,22 @@
+package bounouascancela.sharedobjects;
+
+import java.io.IOException;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+/**
+ * Created by Nassim B on 28/04/17.
+ */
+public class CommandHelp implements Serializable{
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+
+    }
+
+    private void readObjectNoData() throws ObjectStreamException {
+
+    }
+}
