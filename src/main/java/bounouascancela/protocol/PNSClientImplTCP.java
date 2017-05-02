7f2b0c7279e2fd56a1c9e0d711e35d4a4b5e0e45
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-05-02 10:53:04 +0200

    Input loop in PNS client

--- src/main/java/bounouascancela/protocol/PNSClientImplTCP.java
+++ src/main/java/bounouascancela/protocol/PNSClientImplTCP.java
@@ -1,9 +1,36 @@
 package bounouascancela.protocol;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Class x
  *
  * @author Joël CANCELA VAZ
+ * @author Nassim B.
  */
 public class PNSClientImplTCP extends Thread implements PNSClient {
+
+    Scanner ScanIn =  new Scanner(System.in);
+    private int port;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private ObjectInputStream bIn;             // Buffer entree en mode "bytes"
+    private ObjectOutputStream bOut;            // Buffer sortie en mode "bytes"
+
+    public static void main(String[] args) {
+        System.out.println("IN CLIENT");
+        PNSClientImplTCP main = new PNSClientImplTCP();
+        while (true){
+            System.out.printf("### >");
+            String inputted = main.ScanIn.nextLine();
+            System.out.println("You entered : " + inputted);
+        }
+    }
 }
