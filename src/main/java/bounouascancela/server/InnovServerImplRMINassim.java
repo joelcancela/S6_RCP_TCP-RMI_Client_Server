dcc9be305359570bf4b6f94b906a8779e29a2383
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-05-19 00:23:30 +0200

    RMI interface and Implementation to limit numbers of shared objects

---
+++ src/main/java/bounouascancela/server/InnovServerImplRMINassim.java
@@ -0,0 +1,7 @@
+package bounouascancela.server;
+
+/**
+ * Created by Nassim Bounouas on 18/05/2017.
+ */
+public class InnovServerImplRMINassim {
+}
---
+++ src/main/java/bounouascancela/server/InnovServerRMINassim.java
@@ -0,0 +1,10 @@
+package bounouascancela.server;
+
+import java.rmi.Remote;
+
+/**
+ * Created by Nassim Bounouas on 19/05/2017.
+ */
+public interface InnovServerRMINassim extends Remote {
+
+}
